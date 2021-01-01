f6f966951dcf3adda04de23548f6a4d7c01dd3f6
oogeyman <Manish.pal1111@gmail.com>
2021-01-01 15:42:15 +0530

    second push

--- src/com/Test/mvc/Employee.java
+++ src/com/Test/mvc/Employee.java
@@ -39,8 +39,5 @@ public class Employee
 		this.eCity = eCity;
 	}
 
-	Employee()
-	{
 
-	}
 }
--- src/com/Test/mvc/MyController.java
+++ src/com/Test/mvc/MyController.java
@@ -1,17 +1,16 @@
-package com.task.mvc;
+package com.Test.mvc;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 
-import javax.servlet.http.HttpServletRequest;
 
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.ModelMap;
+
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.bind.annotation.RestController;
+
 
 @RestController
 public class MyController 
@@ -24,8 +23,26 @@ public class MyController
 
 	@RequestMapping(value ="/myreg",method = RequestMethod.POST)
 	public String performLogin(@ModelAttribute("user") Employee user)  
-		 Class.forName();
-                    code of Sql
+	{
+	try
+		{
+			Class.forName("com.mysql.jdbc.Driver");
+			Connection con=DriverManager.getConnection("jdbc:mysql://localhost:3306/springmvc","root","root");
+
+
+			PreparedStatement ps=con.prepareStatement("insert into data (eEmail,eName,ePass,eCity) values(?,?,?,?)");
+
+			ps.setString(1, user.geteEmail());
+			ps.setString(2, user.geteName());
+			ps.setString(3, user.getePass());
+			ps.setString(4, user.geteCity());
+			int row=ps.executeUpdate();
+				
+		}
+		catch(Exception e)
+		{
+			e.printStackTrace();
+		}
 		return "home";
 	}  
  
